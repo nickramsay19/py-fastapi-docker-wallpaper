57c2e2d7d1c3a80a90327f3a8558c0271f2c1db4
nickramsay19 <nickramsay19@gmail.com>
2023-05-20 15:29:35 +1000

    adding phone path for portrait images, better auth via headers

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import Any, Callable
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 from urllib.parse import urlencode, urlparse, urlunparse, parse_qsl
 import os # access API_KEY environment variable
 import json # parse the json response
@@ -26,22 +26,53 @@ def generate_url_with_params(base_url: str, params: dict[str, str]) -> str:
     
     return new_url
 
-# generate url
-URL: str = "https://api.unsplash.com/photos/random"
-PARAMS: dict[str, str] = {
-    "topics": "bo8jQKTaE0Y",
-    "orientation": "landscape",
-    "client_id": os.environ["API_KEY"]
-}
+RANDOM_IMAGE_URL: str = "https://api.unsplash.com/photos/random"
+API_KEY = os.environ["API_KEY"]
 
-req_url: str = generate_url_with_params(URL, PARAMS)
+TOPICS: dict[str, str] = {
+    "nature": "62MVjTLSkeQ",
+    "wallpapers": "bo8jQKTaE0Y",
+    "street-photography": "",
+    "experimental": ""
+}
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    with urlopen(req_url) as req:
+    req_url: str = generate_url_with_params(RANDOM_IMAGE_URL, {
+        "topics": "bo8jQKTaE0Y",
+        "orientation": "landscape",
+    })
+
+    auth_req = Request(req_url)
+    auth_req.add_header("Authorization", f"Client-ID {API_KEY}")
+
+    with urlopen(auth_req) as req:
         res: str = req.read()
+
+        # parse json output and extract raw image url
         parsed = json.loads(res)
         img_raw_url = parsed["urls"]["raw"]
+
+        return RedirectResponse(url=img_raw_url)
+
+@app.get("/phone")
+def root():
+
+    req_url: str = generate_url_with_params(RANDOM_IMAGE_URL, {
+        "topics": "bo8jQKTaE0Y",
+        "orientation": "portrait",
+    })
+
+    auth_req = Request(req_url)
+    auth_req.add_header("Authorization", f"Client-ID {API_KEY}")
+
+    with urlopen(auth_req) as req:
+        res: str = req.read()
+        
+        # parse json output and extract raw image url
+        parsed = json.loads(res)
+        img_raw_url = parsed["urls"]["raw"]
+
         return RedirectResponse(url=img_raw_url)
