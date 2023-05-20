9a86b6e3da8df0d3664341624d4ae237fcb800d0
nickramsay19 <nickramsay19@gmail.com>
2023-05-20 13:50:43 +1000

    adding env pass into docker build

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,8 @@ RUN pip install --no-cache-dir --upgrade -r ./requirements.txt
 
 COPY ./app ./app
 
+# you must load the API_KEY value when building
+ARG API_KEY
+ENV API_KEY=${API_KEY}
+
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
 docker container stop wallpaper && docker container rm wallpaper
-echo hey
 
-docker build -t wallpaper-nickramsay-dev $DIR
+# get API_KEY
+source $DIR"/.env"
+
+docker build --build-arg API_KEY=$API_KEY -t wallpaper-nickramsay-dev $DIR
