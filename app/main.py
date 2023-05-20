228ee875de784dedb3cee8194e176125f5d95e77
nickramsay19 <nickramsay19@gmail.com>
2023-05-20 17:51:55 +1000

    shows image directly instead of redirecting

--- app/main.py
+++ app/main.py
@@ -1,36 +1,16 @@
 from __future__ import annotations
 from typing import Any, Callable
-from fastapi import FastAPI, Response
-from fastapi.responses import RedirectResponse
+from fastapi import FastAPI, Response 
+from fastapi.responses import RedirectResponse, StreamingResponse, FileResponse
 from urllib.request import urlopen, Request
-from urllib.parse import urlencode, urlparse, urlunparse, parse_qsl
 import os # access API_KEY environment variable
 import json # parse the json response
-import re # for splitting path names by multiple delimiters
 import logging
+import io
+from .utils import generate_url_with_params, get_topic_slugs_or, img_response
 
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-
-async def generate_url_with_params(base_url: str, params: dict[str, str]) -> str:
-    """generates a raw parameterised url from a param dict"""
-
-    # parse the base URL
-    parsed_url = urlparse(base_url)
-    
-    # get the existing query parameters (if any)
-    existing_params = dict(parse_qsl(parsed_url.query))
-    
-    # merge the existing and new parameters
-    merged_params = {**existing_params, **params}
-    
-    # encode the parameters
-    encoded_params = urlencode(merged_params)
-    
-    # build the new URL with the encoded parameters
-    new_url = urlunparse(parsed_url._replace(query=encoded_params))
-    
-    return new_url
 
 RANDOM_IMAGE_URL: str = "https://api.unsplash.com/photos/random"
 API_KEY = os.environ["API_KEY"]
@@ -42,26 +22,6 @@ TOPICS: dict[str, str] = {
     "experimental": "qPYsDzvJOYc",
     "3d-renders": "CDwuwXJAbEw",
 }
-
-async def get_topic_slugs_or(path: str, topics: dict[str, str], default: str) -> str:
-    """translates our custom user friendly path notation 'topic1+topic2' or 'topic1|topic2' notation to Unsplash's slug query parameter notation '62MVjTLSkeQ,bo8jQKTaE0Y'."""
-    
-    if path == '' or path == None:
-        return default
-
-    topic_names: list[str] = re.split(r'\||\+', path)
-    
-    # attempt to build list of slugs
-    slugs: list[str] = []
-    for t in topic_names:
-        if t in topics.keys():
-            slugs.append(topics[t])
-        else:
-            return None
-
-    # generate unsplash comma delimited query param notation
-    joined = ','.join(slugs)
-    return joined
 
 app = FastAPI()
 
@@ -89,13 +49,14 @@ async def phone(topic_names: str = ''):
         parsed = json.loads(res)
         img_raw_url = parsed["urls"]["raw"]
 
-        return RedirectResponse(url=img_raw_url)
+        return await img_response(img_raw_url)
+        #return RedirectResponse(url=img_raw_url)
 
 @app.get("/")
 async def root():
 
     req_url: str = await generate_url_with_params(RANDOM_IMAGE_URL, {
-        "orientation": "portrait",
+        "orientation": "landscape",
         "topics": TOPICS['nature'],
     })
 
@@ -103,14 +64,27 @@ async def root():
     auth_req = Request(req_url)
     auth_req.add_header("Authorization", f"Client-ID {API_KEY}")
 
-    with urlopen(auth_req) as req:
-        res: str = req.read()
-
-        # parse json output and extract raw image url
-        parsed = json.loads(res)
+    with urlopen(auth_req) as res:
+        parsed = json.loads(res.read())
         img_raw_url = parsed["urls"]["raw"]
 
-        return RedirectResponse(url=img_raw_url)
+        logger.info(img_raw_url)
+
+        img_res = urlopen(img_raw_url) 
+        logger.info(dir(img_res))
+        logger.info(img_res)
+
+        content = io.BytesIO(img_res.read())
+        content.seek(0)
+        #logger.info(content)
+
+        if img_res.getcode() == 200:
+            return StreamingResponse(iter(content.read, b""), media_type="image/jpeg")            
+        else:
+            return Response(f"error: {img_res.getcode()}")
+
+        #return await img_response(img_raw_url)
+        
 
 @app.get("/{topic_names}")
 async def root_with_topics(topic_names: str):
@@ -120,7 +94,7 @@ async def root_with_topics(topic_names: str):
         return Response(f"Error: Invalid topic names.\n\nHey dumb dumb, valid topic names are: {', '.join(TOPICS.keys())}.\nYou may delimit topic names via a \"+\" or \"|\" symbol.\nAdditionally, you may leave the topics blank to default to a the \"nature\" topic.")
 
     req_url: str = await generate_url_with_params(RANDOM_IMAGE_URL, {
-        "orientation": "portrait",
+        "orientation": "landscape",
         "topics": topic_slugs,
     })
 
@@ -135,6 +109,6 @@ async def root_with_topics(topic_names: str):
         parsed = json.loads(res)
         img_raw_url = parsed["urls"]["raw"]
 
-        return RedirectResponse(url=img_raw_url)
+        return await img_response(img_raw_url)
 
 
---
+++ app/utils.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+from typing import Any, Callable
+from fastapi import Response
+from fastapi.responses import StreamingResponse
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode, urlparse, urlunparse, parse_qsl
+import re # for splitting path names by multiple delimiters
+import io # for io.BytesIO
+
+async def generate_url_with_params(base_url: str, params: dict[str, str]) -> str:
+    """generates a raw parameterised url from a param dict"""
+
+    # parse the base URL
+    parsed_url = urlparse(base_url)
+    
+    # get the existing query parameters (if any)
+    existing_params = dict(parse_qsl(parsed_url.query))
+    
+    # merge the existing and new parameters
+    merged_params = {**existing_params, **params}
+    
+    # encode the parameters
+    encoded_params = urlencode(merged_params)
+    
+    # build the new URL with the encoded parameters
+    new_url = urlunparse(parsed_url._replace(query=encoded_params))
+    
+    return new_url
+
+async def get_topic_slugs_or(path: str, topics: dict[str, str], default: str) -> str:
+    """translates our custom user friendly path notation 'topic1+topic2' or 'topic1|topic2' notation to Unsplash's slug query parameter notation '62MVjTLSkeQ,bo8jQKTaE0Y'."""
+    
+    if path == '' or path == None:
+        return default
+
+    topic_names: list[str] = re.split(r'\||\+', path)
+    
+    # attempt to build list of slugs
+    slugs: list[str] = []
+    for t in topic_names:
+        if t in topics.keys():
+            slugs.append(topics[t])
+        else:
+            return None
+
+    # generate unsplash comma delimited query param notation
+    joined = ','.join(slugs)
+    return joined
+
+async def img_response(img_url: str) -> StreamingResponse | Response:
+
+    res = urlopen(img_url) 
+
+    if res.getcode() == 200:
+        # create byte iterator
+        img_bytes = io.BytesIO(res.read())
+        img_bytes.seek(0)
+        img_bytes_iter = iter(img_bytes.read, b"")
+
+        return StreamingResponse(img_bytes_iter, media_type="image/jpeg")            
+    else:
+        return Response(f"Error: In retrieving image from \"{img_url}\", got error code: {img_res.getcode()}.")
+
